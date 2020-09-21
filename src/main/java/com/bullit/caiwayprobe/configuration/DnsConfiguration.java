36982b0ad4d34eef305d16501df484c7c606649f
jonckvanderkogel <jonckvanderkogel@gmail.com>
2020-09-21 12:39:32 +0200

    externalized properties and fixed a bug which would return not reachable even if the fallback would be reachable

---
+++ src/main/java/com/bullit/caiwayprobe/configuration/DnsConfiguration.java
@@ -0,0 +1,24 @@
+package com.bullit.caiwayprobe.configuration;
+
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "dns")
+public class DnsConfiguration {
+    private String serverOne;
+    private String serverTwo;
+
+    @Bean(name="dnsServerOne")
+    public String getServerOne() {
+        return this.serverOne;
+    }
+
+    @Bean(name="dnsServerTwo")
+    public String getServerTwo() {
+        return this.serverTwo;
+    }
+}
--- src/main/java/com/bullit/caiwayprobe/service/PingService.java
+++ src/main/java/com/bullit/caiwayprobe/service/PingService.java
@@ -3,11 +3,11 @@ package com.bullit.caiwayprobe.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -16,18 +16,24 @@ import java.util.stream.Stream;
 
 @Slf4j
 @Service
+@ConfigurationProperties(prefix = "dns")
 public class PingService {
-    private static final String DNS_1 = "62.45.70.117";
-    private static final String DNS_2 = "62.45.71.117";
+    private final String serverOne;
+    private final String serverTwo;
     private final Executor pingExecutor;
 
-    public PingService(@Autowired @Qualifier("pingExecutor") Executor pingExecutor) {
+    public PingService(@Autowired @Qualifier("pingExecutor") Executor pingExecutor,
+                       @Autowired @Qualifier("dnsServerOne") String dnsServerOne,
+                       @Autowired @Qualifier("dnsServerTwo") String dnsServerTwo) {
+        log.info(String.format("Starting up PingService with servers: %s and %s", dnsServerOne, dnsServerTwo));
         this.pingExecutor = pingExecutor;
+        this.serverOne = dnsServerOne;
+        this.serverTwo = dnsServerTwo;
     }
 
     public CompletableFuture<PingResponse> pingDnsServers() {
-        var pingFuture1 = CompletableFuture.supplyAsync(() -> performPing(DNS_1), pingExecutor);
-        var pingFuture2 = CompletableFuture.supplyAsync(() -> performPing(DNS_2), pingExecutor);
+        var pingFuture1 = CompletableFuture.supplyAsync(() -> performPing(serverOne), pingExecutor);
+        var pingFuture2 = CompletableFuture.supplyAsync(() -> performPing(serverTwo), pingExecutor);
 
         return CompletableFuture
                 .allOf(pingFuture1, pingFuture2)
@@ -35,7 +41,7 @@ public class PingService {
                         .join()
                         .orElseGet(() -> pingFuture2
                                 .join()
-                                .orElseGet(() ->new PingResponse(false, 400))
+                                .orElseGet(() ->new PingResponse(false, 666))
                         )
                 );
     }
@@ -44,10 +50,10 @@ public class PingService {
         try {
             InetAddress address = InetAddress.getByName(ipAddress);
             Date now = new Date();
-            boolean isReachable = address.isReachable(400);
+            boolean isReachable = address.isReachable(900);
             long timeToRespond = new Date().getTime() - now.getTime();
 
-            return Optional.of(new PingResponse(isReachable, timeToRespond));
+            return isReachable ? Optional.of(new PingResponse(isReachable, timeToRespond)) : Optional.empty();
         } catch (IOException e) {
             log.error(e.getMessage());
             Stream.of(e.getStackTrace()).forEach(l -> log.error(l.toString()));
