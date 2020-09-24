3cc144d025a141f08731028bfa5e0ce23a06c91d
jonckvanderkogel <jonckvanderkogel@gmail.com>
2020-09-24 18:16:59 +0200

    rewrote the MDC logger javadoc to be a bit more clear

--- src/main/java/com/bullit/networkprobe/support/MDCLogger.java
+++ src/main/java/com/bullit/networkprobe/support/MDCLogger.java
@@ -3,7 +3,7 @@ package com.bullit.networkprobe.support;
 import org.slf4j.MDC;
 
 /**
-Since Reactive Streams does not play nice with Thread Local implementations such as MDC we have to make sure the MDC
+Since Reactive Streams processes are not guaranteed to run on any specific thread we have to make sure the MDC
  context is cleared before logging.
  */
 public class MDCLogger {
