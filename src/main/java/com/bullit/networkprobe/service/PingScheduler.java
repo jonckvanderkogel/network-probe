13a8d227599b3316000279f8b098f2e83fdf2e53
jonckvanderkogel <jonckvanderkogel@gmail.com>
2020-10-03 13:32:54 +0200

    removed unnecessary import

--- src/main/java/com/bullit/networkprobe/service/PingScheduler.java
+++ src/main/java/com/bullit/networkprobe/service/PingScheduler.java
@@ -3,7 +3,6 @@ package com.bullit.networkprobe.service;
 import com.bullit.networkprobe.domain.PingResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
