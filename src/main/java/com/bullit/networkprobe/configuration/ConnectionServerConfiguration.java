6afce4101859c87477e45a6fbe73cabc23aeb9b6
jonckvanderkogel <jonckvanderkogel@gmail.com>
2020-10-18 12:16:29 +0200

    simplified the Bean creation mechanism further

--- src/main/java/com/bullit/networkprobe/configuration/ConnectionServerConfiguration.java
+++ src/main/java/com/bullit/networkprobe/configuration/ConnectionServerConfiguration.java
@@ -1,8 +1,11 @@
 package com.bullit.networkprobe.configuration;
 
+import com.bullit.networkprobe.domain.ConnectionResponse;
+import com.bullit.networkprobe.service.ConnectionScheduler;
 import com.bullit.networkprobe.service.ConnectionService;
 import com.bullit.networkprobe.support.MDCLogger;
 import lombok.Setter;
+import org.reactivestreams.Subscriber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Pattern;
+import java.util.List;
 
 @Validated
 @Setter
@@ -25,7 +29,8 @@ public class ConnectionServerConfiguration {
     private Integer timeOutMillis;
 
     @Bean
-    public ConnectionService getConnectionService(@Autowired MDCLogger mdcLogger) {
-        return new ConnectionService(serverOne, serverTwo, timeOutMillis, mdcLogger);
+    public ConnectionScheduler getConnectionScheduler(@Autowired List<Subscriber<ConnectionResponse>> subscribers,
+                                                      @Autowired MDCLogger mdcLogger) {
+        return new ConnectionScheduler(new ConnectionService(serverOne, serverTwo, timeOutMillis, mdcLogger), subscribers);
     }
 }
--- src/main/java/com/bullit/networkprobe/service/ConnectionScheduler.java
+++ src/main/java/com/bullit/networkprobe/service/ConnectionScheduler.java
@@ -3,21 +3,18 @@ package com.bullit.networkprobe.service;
 import com.bullit.networkprobe.domain.ConnectionResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Subscriber;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Slf4j
-@Component
 public class ConnectionScheduler {
     private final ConnectionService testConnectionService;
     private final List<Subscriber<ConnectionResponse>> subscribers;
 
-    public ConnectionScheduler(@Autowired ConnectionService connectionService,
-                               @Autowired List<Subscriber<ConnectionResponse>> subscribers) {
+    public ConnectionScheduler(ConnectionService connectionService,
+                               List<Subscriber<ConnectionResponse>> subscribers) {
         this.testConnectionService = connectionService;
         this.subscribers = subscribers;
     }
