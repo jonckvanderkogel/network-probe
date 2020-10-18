9f207a296dc7d288cb443ab20be9ab3faef74f87
jonckvanderkogel <jonckvanderkogel@gmail.com>
2020-10-18 12:20:59 +0200

    removed some unnecessary annotations

--- src/main/java/com/bullit/networkprobe/domain/ConnectionResponse.java
+++ src/main/java/com/bullit/networkprobe/domain/ConnectionResponse.java
@@ -1,10 +1,12 @@
 package com.bullit.networkprobe.domain;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
-@Data
+@ToString
+@EqualsAndHashCode
 @Getter
 @RequiredArgsConstructor
 public class ConnectionResponse {
--- src/main/java/com/bullit/networkprobe/service/ConnectionScheduler.java
+++ src/main/java/com/bullit/networkprobe/service/ConnectionScheduler.java
@@ -1,14 +1,12 @@
 package com.bullit.networkprobe.service;
 
 import com.bullit.networkprobe.domain.ConnectionResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Subscriber;
 import reactor.core.publisher.Flux;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
-@Slf4j
 public class ConnectionScheduler {
     private final ConnectionService testConnectionService;
     private final List<Subscriber<ConnectionResponse>> subscribers;
